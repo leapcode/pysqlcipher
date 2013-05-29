1389e9c5769a34c1908e2104514e8b0f5551f7c6
Kali Kaneko <kali@leap.se>
2013-05-30 04:58:46 +0900

    bump dev version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ sqlite = "sqlite"
 PYSQLITE_EXPERIMENTAL = False
 
 #DEV_VERSION = None
-DEV_VERSION = "1"
+DEV_VERSION = "2"
 
 sources = ["src/module.c", "src/connection.c", "src/cursor.c", "src/cache.c",
            "src/microprotocols.c", "src/prepare_protocol.c", "src/statement.c",
