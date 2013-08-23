698c4cbf18933cf083b543806b5a6e11019a90da
Kali Kaneko <kali@leap.se>
2013-08-23 12:42:02 +0200

    improve packaging

--- mail/setup.py
+++ mail/setup.py
@@ -17,27 +17,41 @@
 """
 setup file for leap.mail
 """
+from setuptools import setup
+from setuptools import find_packages
 
+import versioneer
+versioneer.versionfile_source = 'src/leap/mail/_version.py'
+versioneer.versionfile_build = 'leap/mail/_version.py'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'leap.mail-'
 
-from setuptools import setup, find_packages
+from pkg import utils
 
-
-requirements = [
-    "leap.soledad.client>=0.3.0",
-    "leap.common>=0.2.3-dev",
-    "leap.keymanager>=0.2.0",
-    "twisted",
+trove_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Framework :: Twisted',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License '
+    'v3 (GPLv3)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Communications :: Email',
+    'Topic :: Communications :: Email :: Post-Office :: IMAP',
+    'Topic :: Communications :: Email :: Post-Office :: POP3',
+    'Topic :: Internet',
+    'Topic :: Security :: Cryptography',
+    'Topic :: Software Development :: Libraries',
 ]
 
-tests_requirements = [
-    'setuptools-trial',
-    'mock',
-]
+# XXX add ref to docs
 
-# XXX add classifiers, docs
 setup(
     name='leap.mail',
-    version='0.3.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     url='https://leap.se/',
     license='GPLv3+',
     author='The LEAP Encryption Access Project',
@@ -46,10 +60,12 @@ setup(
     long_description=(
         "Mail Services in the LEAP Client project."
     ),
+    classifiers=trove_classifiers,
     namespace_packages=["leap"],
     package_dir={'': 'src'},
     packages=find_packages('src'),
     test_suite='leap.mail.load_tests',
-    install_requires=requirements,
-    tests_require=tests_requirements,
+    install_requires=utils.parse_requirements(),
+    tests_require=utils.parse_requirements(
+        reqfiles=['pkg/requirements-testing.pip']),
 )
