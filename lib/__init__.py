0b227fa63048eaabbdfa457044ef651e784a03b6
cyBerta <richy@cyborgsociety.org>
2014-02-13 03:01:28 +0100

    compile eggs for android

--- lib/__init__.py
+++ lib/__init__.py
@@ -20,3 +20,8 @@
 # 2. Altered source versions must be plainly marked as such, and must not be
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
+
+from ctypes import *
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+lib = cdll.LoadLibrary(path+"/libsqlcipher.so")
--- setup.py
+++ setup.py
@@ -21,8 +21,6 @@
 # 2. Altered source versions must be plainly marked as such, and must not be
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
-
-import glob
 import os
 import re
 import sys
@@ -31,15 +29,21 @@ import zipfile
 
 from types import ListType, TupleType
 
-from distutils.core import setup, Extension, Command
-#from setuptools import setup, Extension, Command
+#from distutils.core import setup, Extension, Command
+from setuptools import setup, Extension, Command
 from distutils.command.build import build
-from distutils.command.build_ext import build_ext
+#from distutils.command.build_ext import build_ext
 from distutils.dep_util import newer_group
 from distutils.errors import DistutilsSetupError
 from distutils import log
 
 import cross_bdist_wininst
+
+#YOU'LL NEED THIS: https://python-for-android.googlecode.com/files/python-lib_r16.zip AND THIS DESCRIPTION WILL HELP, TOO: 
+# https://code.google.com/p/python-for-android/wiki/BuildingModules
+
+from py4a import patch_distutils 
+patch_distutils()
 
 # If you need to change anything, it should be enough to change setup.cfg.
 
@@ -47,17 +51,19 @@ sqlite = "sqlite"
 
 PYSQLITE_EXPERIMENTAL = False
 
-#DEV_VERSION = None
-DEV_VERSION = "1"
+DEV_VERSION = None
+#DEV_VERSION = "02"
 
 sources = ["src/module.c", "src/connection.c", "src/cursor.c", "src/cache.c",
            "src/microprotocols.c", "src/prepare_protocol.c", "src/statement.c",
            "src/util.c", "src/row.c"]
+depends = []
 
+           
 if PYSQLITE_EXPERIMENTAL:
     sources.append("src/backup.c")
 
-include_dirs = []
+include_dirs = ["amalgamation"] 
 library_dirs = []
 libraries = []
 runtime_library_dirs = []
@@ -110,8 +116,7 @@ def get_amalgamation():
     os.mkdir(AMALGAMATION_ROOT)
     print "Downloading amalgation."
 
-    # XXX upload the amalgamation file to a somewhat more
-    # official place
+    # XXX upload the amalgamation file to downloads.leap.se
     amalgamation_url = ("http://futeisha.org/sqlcipher/"
                         "amalgamation-sqlcipher-2.1.0.zip")
 
@@ -131,149 +136,6 @@ def get_amalgamation():
     zf.close()
     os.unlink("tmp.zip")
 
-
-class AmalgamationBuilder(build):
-    description = ("Build a statically built pysqlcipher "
-                   "downloading and using a sqlcipher amalgamation.")
-
-    def __init__(self, *args, **kwargs):
-        MyBuildExt.amalgamation = True
-        MyBuildExt.static = True
-        build.__init__(self, *args, **kwargs)
-
-
-class MyBuildExt(build_ext):
-    amalgamation = True  # We want amalgamation on the default build for now
-    static = False
-
-    def build_extension(self, ext):
-        if self.amalgamation:
-            get_amalgamation()
-            # build with fulltext search enabled
-            ext.define_macros.append(
-                ("SQLITE_ENABLE_FTS3", "1"))
-            ext.define_macros.append(
-                ("SQLITE_ENABLE_RTREE", "1"))
-
-            # SQLCipher options
-            ext.define_macros.append(
-                ("SQLITE_ENABLE_LOAD_EXTENSION", "1"))
-            ext.define_macros.append(
-                ("SQLITE_HAS_CODEC", "1"))
-            ext.define_macros.append(
-                ("SQLITE_TEMP_STORE", "2"))
-
-            ext.sources.append(os.path.join(AMALGAMATION_ROOT, "sqlite3.c"))
-            ext.include_dirs.append(AMALGAMATION_ROOT)
-
-            ext.extra_link_args.append("-lcrypto")
-
-        if self.static:
-            self._build_extension(ext)
-        else:
-            build_ext.build_extension(self, ext)
-
-    def _build_extension(self, ext):
-        sources = ext.sources
-        if sources is None or type(sources) not in (ListType, TupleType):
-            raise DistutilsSetupError, \
-                ("in 'ext_modules' option (extension '%s'), " +
-                 "'sources' must be present and must be " +
-                 "a list of source filenames") % ext.name
-        sources = list(sources)
-
-        ext_path = self.get_ext_fullpath(ext.name)
-        depends = sources + ext.depends
-        if not (self.force or newer_group(depends, ext_path, 'newer')):
-            log.debug("skipping '%s' extension (up-to-date)", ext.name)
-            return
-        else:
-            log.info("building '%s' extension", ext.name)
-
-        # First, scan the sources for SWIG definition files (.i), run
-        # SWIG on 'em to create .c files, and modify the sources list
-        # accordingly.
-        sources = self.swig_sources(sources, ext)
-
-        # Next, compile the source code to object files.
-
-        # XXX not honouring 'define_macros' or 'undef_macros' -- the
-        # CCompiler API needs to change to accommodate this, and I
-        # want to do one thing at a time!
-
-        # Two possible sources for extra compiler arguments:
-        #   - 'extra_compile_args' in Extension object
-        #   - CFLAGS environment variable (not particularly
-        #     elegant, but people seem to expect it and I
-        #     guess it's useful)
-        # The environment variable should take precedence, and
-        # any sensible compiler will give precedence to later
-        # command line args.  Hence we combine them in order:
-        extra_args = ext.extra_compile_args or []
-
-        macros = ext.define_macros[:]
-        for undef in ext.undef_macros:
-            macros.append((undef,))
-
-        # XXX debug
-        #objects = []
-        objects = self.compiler.compile(sources,
-                                        output_dir=self.build_temp,
-                                        macros=macros,
-                                        include_dirs=ext.include_dirs,
-                                        debug=self.debug,
-                                        extra_postargs=extra_args,
-                                        depends=ext.depends)
-
-        # XXX -- this is a Vile HACK!
-        #
-        # The setup.py script for Python on Unix needs to be able to
-        # get this list so it can perform all the clean up needed to
-        # avoid keeping object files around when cleaning out a failed
-        # build of an extension module.  Since Distutils does not
-        # track dependencies, we have to get rid of intermediates to
-        # ensure all the intermediates will be properly re-built.
-        #
-        self._built_objects = objects[:]
-
-        # Now link the object files together into a "shared object" --
-        # of course, first we have to figure out all the other things
-        # that go into the mix.
-        if ext.extra_objects:
-            objects.extend(ext.extra_objects)
-        extra_args = ext.extra_link_args or []
-
-        # Detect target language, if not provided
-        language = ext.language or self.compiler.detect_language(sources)
-
-        #self.compiler.link_shared_object(
-            #objects, ext_path,
-            #libraries=self.get_libraries(ext),
-            #library_dirs=ext.library_dirs,
-            #runtime_library_dirs=ext.runtime_library_dirs,
-            #extra_postargs=extra_args,
-            #export_symbols=self.get_export_symbols(ext),
-            #debug=self.debug,
-            #build_temp=self.build_temp,
-            #target_lang=language)
-
-        # XXX may I have a static lib please?
-        # hmm but then I cannot load that extension, or can I?
-        output_dir = os.path.sep.join(ext_path.split(os.path.sep)[:-1])
-
-        self.compiler.create_static_lib(
-            objects,
-            #XXX get library name ... splitting ext_path?
-            "sqlite",
-            output_dir=output_dir,
-            target_lang=language)
-
-    def __setattr__(self, k, v):
-        # Make sure we don't link against the SQLite
-        # library, no matter what setup.cfg says
-        if self.amalgamation and k == "libraries":
-            v = None
-        self.__dict__[k] = v
 
 
 def get_setup_args():
@@ -297,15 +159,9 @@ def get_setup_args():
         print "Fatal error: PYSQLITE_VERSION could not be detected!"
         sys.exit(1)
 
-    data_files = [("pysqlcipher-doc",
-                  glob.glob("doc/*.html")
-                  + glob.glob("doc/*.txt")
-                  + glob.glob("doc/*.css")),
-                  ("pysqlcipher-doc/code",
-                  glob.glob("doc/code/*.py"))]
-
-    py_modules = ["sqlcipher"],
-
+    library_dirs=["lib"]
+    libraries=["sqlcipher"]   
+    
     setup_args = dict(
         name="pysqlcipher",
         version=PYSQLITE_VERSION,
@@ -317,19 +173,18 @@ def get_setup_args():
         license="zlib/libpng",  # is THIS a license?
         # It says MIT in the google project
         platforms="ALL",
-        #XXX missing url
         url="http://github.com/leapcode/pysqlcipher/",
         # Description of the modules and packages in the distribution
         package_dir={"pysqlcipher": "lib"},
+        package_data={'pysqlcipher': ['*.so']},
         packages=["pysqlcipher", "pysqlcipher.test"] +
             (["pysqlcipher.test.py25"], [])[sys.version_info < (2, 5)],
         scripts=[],
-        data_files=data_files,
-
         ext_modules=[
             Extension(
                 name="pysqlcipher._sqlite",
                 sources=sources,
+                depends=depends,
                 include_dirs=include_dirs,
                 library_dirs=library_dirs,
                 runtime_library_dirs=runtime_library_dirs,
@@ -338,7 +193,7 @@ def get_setup_args():
                 define_macros=define_macros)
         ],
         classifiers=[
-            "Development Status :: 5 - Production/Stable",
+            "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: zlib/libpng License",
             "Operating System :: MacOS :: MacOS X",
@@ -352,14 +207,13 @@ def get_setup_args():
     )
 
     setup_args["cmdclass"].update(
-        {"build_docs": DocBuilder,
-         "build_ext": MyBuildExt,
-         "build_static": AmalgamationBuilder,
-         "cross_bdist_wininst": cross_bdist_wininst.bdist_wininst})
+        {"build_docs": DocBuilder
+        })
     return setup_args
 
 
 def main():
+    get_amalgamation()
     setup(**get_setup_args())
 
 if __name__ == "__main__":
