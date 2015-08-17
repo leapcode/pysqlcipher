399b5518deb6bea155042a46cbcbb62d743bc4db
Kali Kaneko <kali@leap.se>
2015-08-17 19:22:14 -0400

    [style] pep8 fix

--- keymanager/src/leap/keymanager/openpgp.py
+++ keymanager/src/leap/keymanager/openpgp.py
@@ -111,7 +111,8 @@ class TempGPGWrapper(object):
         # and we want to count the keys afterwards.
 
         privids = map(lambda privkey: privkey.key_id, privkeys)
-        publkeys = filter(lambda pubkey: pubkey.key_id not in privids, publkeys)
+        publkeys = filter(
+            lambda pubkey: pubkey.key_id not in privids, publkeys)
 
         listkeys = lambda: self._gpg.list_keys()
         listsecretkeys = lambda: self._gpg.list_keys(secret=True)
