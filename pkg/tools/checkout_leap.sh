3c9f936d95cfaebef26e84332367beff30701612
Kali Kaneko (leap communications) <kali@leap.se>
2016-09-20 12:58:07 -0400

    [tests] add script to checkout common/soledad

---
+++ pkg/tools/checkout_leap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -d "../leap_commondev" ]; then
+	git clone https://github.com/leapcode/leap_pycommon ../leap_commondev
+fi
+if [ ! -d "../soledaddev" ]; then
+	git clone https://github.com/leapcode/soledad ../soledaddev
+fi
+cd ../leap_commondev && git checkout develop && git pull origin develop
+cd ../soledaddev && git checkout develop && git pull origin develop
