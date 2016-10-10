c1fdf7dfa3ba6f7eeca2a1f980ab4a68da735654
drebs <drebs@riseup.net>
2016-10-10 13:20:08 -0300

    [bug] create log directory on log initialization

--- src/leap/bitmask/core/logs.py
+++ src/leap/bitmask/core/logs.py
@@ -1,5 +1,5 @@
 from os import makedirs
-from os.path import abspath, join, isfile
+from os.path import abspath, join, isfile, isdir
 
 from twisted.python import logfile
 
@@ -8,7 +8,7 @@ from leap.common.config import get_path_prefix
 
 def loggerFactory():
     configdir = abspath(join(get_path_prefix(), 'leap'))
-    if not configdir:
+    if not isdir(configdir):
         makedirs(configdir)
     log_path = abspath(join(get_path_prefix(), 'leap', 'bitmaskd.log'))
     rotate = isfile(log_path)
